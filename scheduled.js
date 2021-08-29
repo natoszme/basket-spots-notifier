6fda20cb272b42c11d9d21cef14feba90fba1d46
Nato Szmedra <nato@producteca.com>
2021-08-29 14:41:15 -0300

    feat: add sundays to scheduled job days

--- scheduled.js
+++ scheduled.js
@@ -1,5 +1,5 @@
 const dayOfWeek = new Date().getDay();
-if (dayOfWeek == 2 || dayOfWeek == 5)
+if (dayOfWeek == 2 || dayOfWeek == 5 || dayOfWeek == 0)
   require("./index");
 else
   console.log("Skipping task run");
